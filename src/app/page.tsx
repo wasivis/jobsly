0944140f3b2b1d4a6e550c81703b8d0cdfbf64e2
Ivan <ivanmontequin@gmail.com>
2026-02-25 11:39:19 -0300

    Remove Stripe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,12 +85,6 @@ export default async function Home({ searchParams }: HomeProps) {
               Jobsly
             </h1>
           </div>
-
-          <Link href="/post">
-            <button className="cursor-pointer bg-indigo-600 text-white px-5 py-2 rounded-xl text-sm font-bold hover:bg-indigo-500 transition shadow-sm">
-              Post a Job
-            </button>
-          </Link>
         </div>
       </nav>
 
